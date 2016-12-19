c6c0ff4e8be9631101d260159d8fcd7dcf9d3dd8
NBerlin <nickiberlinpolhem@gmail.com>
2016-12-19 15:31:13 +0100

    Övning 3 hälften klart
    Fortfarande kvar en förklaring och en Iterator

--- ovn3/src/arraycollection/ArrayCollection.java
+++ ovn3/src/arraycollection/ArrayCollection.java
@@ -1,6 +1,5 @@
 package arraycollection;
 
-
 import java.util.Collection;
 import java.util.Iterator;
 
@@ -14,9 +13,11 @@ public class ArrayCollection<E> implements Collection<E> {
 		theCollection = (E[]) new Object[10];
 		size = 0;
 	}
-	
-	/** Appends the specified element to the end of this list.	   
-    Returns true if this collection changed as a result of the call. */
+
+	/**
+	 * Appends the specified element to the end of this list. Returns true if
+	 * this collection changed as a result of the call.
+	 */
 	public boolean add(E x) {
 		if (size == theCollection.length) {
 			doubleArray();
@@ -25,19 +26,36 @@ public class ArrayCollection<E> implements Collection<E> {
 		size++;
 		return true;
 	}
-	
+
 	/** Returns true if this collection contains the specified element. */
 	public boolean contains(Object x) {
-		// Fyll i egen kod
+		// Fett basic men borde klara vad de söker efter.
+		for (int i = 0; i < size; i++) {
+			if (theCollection[i] == x) {
+				return true;
+			}
+		}
 		return false;
 	}
-	
-	/* Creates an new array that is twice the size of the current array and 
-	   copies the content of the current array into the new one. */
+
+	/*
+	 * Creates an new array that is twice the size of the current array and
+	 * copies the content of the current array into the new one.
+	 */
 	private void doubleArray() {
-		// Fyll i egen kod
+		// ny array som är lika stor som nuvarande(fulla) arrayen
+		E[] theTemp = (E[]) new Object[theCollection.length];
+		// nya är nu exakt likadan som gamla
+		theTemp = theCollection;
+		// Gör om ursprungliga till en tom array som är dubbel så stor
+		theCollection = (E[]) new Object[theTemp.length * 2];
+		// För storleken på gamla så sätter man in de gamla värdena i den nya
+		// arrayen
+		for (int i = 0; i < theTemp.length; i++) {
+			theCollection[i] = theTemp[i];
+		}
 	}
-	
+
 	public Iterator<E> iterator() {
 		// Fyll i egen kod
 		return null;
@@ -46,14 +64,14 @@ public class ArrayCollection<E> implements Collection<E> {
 	public int size() {
 		return size;
 	}
-	
+
 	public boolean addAll(Collection<? extends E> c) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public void clear() {
-		// TODO Auto-generated method stub		
+		// TODO Auto-generated method stub
 	}
 
 	public boolean containsAll(Collection<?> c) {
@@ -90,5 +108,5 @@ public class ArrayCollection<E> implements Collection<E> {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 }
