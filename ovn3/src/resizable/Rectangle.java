40a0f8be51c78a028cd30255a225f9a330471ae7
NBerlin <nickiberlinpolhem@gmail.com>
2016-12-19 21:38:12 +0100

    Jobbat vidare lite på övning 3

--- ovn3/src/resizable/Rectangle.java
+++ ovn3/src/resizable/Rectangle.java
@@ -16,18 +16,21 @@ public class Rectangle implements Resizable {
 	//här implementeras downSize U 20
 	public void downSize(int scaleFactor){
 		if(scaleFactor<=0){
-		// Skrev så här ursprungligen för brukar skriva så här
-		//this.height=this.height/scaleFactor;
-		// Men måste börja skriva så här istället
-		this.height/=scaleFactor;
-		this.width/=scaleFactor;
-		}
-		else{
 			//felmeddelande som man skulle skriva i U21. Tror att de har skrivit fel i uppgiften och egentligen
 			//menar >0 för fall man delar på noll går världen sönder och varför ska skalfaktorn göra talet negativt?
-			System.out.println("Du behöver ett tal som är <=0");
+			throw new IllegalArgumentException("Ogiltigt värde på skalfaktorn" + scaleFactor);
 		}
+		else{
+
+			// Skrev så här ursprungligen för brukar skriva så här
+			//this.height=this.height/scaleFactor;
+			// Men måste börja skriva så här istället
+			this.height/=scaleFactor;
+			this.width/=scaleFactor;
+		}
+	
 	}
+	
 	
 
 }
